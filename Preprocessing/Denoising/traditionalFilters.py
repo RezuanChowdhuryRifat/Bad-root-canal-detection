2b29c5b9c8396e8417b96804f22e40c3417fa328
Rezuan Chowdhury Rifat <87327764+RezuanChowdhuryRifat@users.noreply.github.com>
2022-09-07 07:15:46 +0600

    Update traditionalFilters.py

--- Preprocessing/Denoising/traditionalFilters.py
+++ Preprocessing/Denoising/traditionalFilters.py
@@ -22,13 +22,21 @@ import bm3d
 import cv2
 from skimage import io, img_as_float
 
-noisy_image = img_as_float(io.imread("C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/train/ptf (98).jpg" , as_gray= True))
-#noisy_image =img_as_float(io.imread("C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/train/ptf (119).jpg" , as_gray= True))
+PATH = "C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/train/"
+new_PATH ="C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/train2/"
+list = os.listdir("C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/train")
+
+for file in list:
+    noisy_image = img_as_float(io.imread(PATH + file , as_gray= False))
+    denoised_image = bm3d.bm3d(noisy_image, sigma_psd=0.05, stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
+    io.imsave(new_PATH + file,denoised_image)
 
 
-denoised_image = bm3d.bm3d(noisy_image, sigma_psd=0.05, stage_arg=bm3d.BM3DStages.ALL_STAGES)
+PATH = "C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/val/"
+new_PATH ="C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/val2/"
+list = os.listdir("C:/Users/rezua/Documents/GitHub/yolov7/custom_dataset/images/val")
 
-cv2.imshow("Orginal", noisy_image)
-cv2.imshow("Denoised", denoised_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+for file in list:
+    noisy_image = img_as_float(io.imread(PATH + file , as_gray= False))
+    denoised_image = bm3d.bm3d(noisy_image, sigma_psd=0.05, stage_arg=bm3d.BM3DStages.HARD_THRESHOLDING)
+    io.imsave(new_PATH + file,denoised_image)
